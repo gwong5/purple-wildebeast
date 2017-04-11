d5ab0e21b0aad278222dfa5a5a36d22ad64bb9f9
Greg <kousagi1012@yahoo.com>
2017-04-11 14:32:53 -0700

    clean up work files

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require('express')
-const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 
-app.set('view engine', 'pug')
-
-const addBook = require('./routes/routes')
-const indexFile = require('./routes')
-
-app.set('views', path.join(__dirname, 'views'))
-
+const app = express()
 app.listen(3000)
+
+const indexFile = require('./routes/routes')
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', indexFile)
-app.use('/add', addBook)
-
-
-// app.get('/', (request, response) => {
-//     response.render('index')
-// })
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const route = express.Router()
-
-route.get('/', (request, response) => {
-  response.render('index')
-})
-
-module.exports = route
