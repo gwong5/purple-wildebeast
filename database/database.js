d51807aa05df1a35f9ef21e353fc4377fefe47b8
yaseenagag <yaseen.uc@gmail.com>
2017-04-13 11:01:38 -0700

    implement get and create routes

--- database/database.js
+++ database/database.js
@@ -4,7 +4,11 @@ const db = pgp({database: 'bookstore'})
 
 const createBook = (title, author, genre, image, description) => db.none('INSERT INTO books (title, author, genre, image, description) VALUES($1, $2, $3, $4, $5)', [title, author, genre, image, description])
 
-const getBook = () => db.any('SELECT * FROM books')
+const getAllBooks = () => db.any('SELECT * FROM books')
+
+const getBook = (id) => db.any('SELECT * FROM books WHERE ID = $1', [id])
 
 
-module.exports = { createBook, getBook }
+
+
+module.exports = { createBook, getAllBooks, getBook }
--- routes/routes.js
+++ routes/routes.js
@@ -4,19 +4,21 @@ const db = require('../database/database')
 
 router.get('/', (request, response) => {
 const { title, author, genre, image, description } = request.body
-console.log('------------------------------------');
-console.log(request.body);
-console.log('------------------------------------');
-  db.getBook()
+  db.getAllBooks()
     .then((books) => response.render('index', {books}))
+    .catch(error => response.send({ error, message: error.message }))
+})
+
+router.get('/details/:id', (request, response) => {
+const { title, author, genre, image, description } = request.body
+const {id} = request.params
+  db.getBook(id)
+    .then((book) => response.render('details', {book}))
     .catch(error => response.send({ error, message: error.message }))
 })
 
 router.post('/add', (request, response) => {
 const { title, author, genre, image, description } = request.body
-console.log('------------------------------------');
-console.log(request.body);
-console.log('------------------------------------');
 db.createBook(title, author, genre, image, description)
     .then(() => response.redirect('/'))
     .catch(error => response.send({ error, message: error.message }))
