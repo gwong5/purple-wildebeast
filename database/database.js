fd2513f6aa8a002fcc09f91c7c55b97a84c6b221
Greg <kousagi1012@yahoo.com>
2017-04-14 15:05:53 -0700

    update error image

--- database/database.js
+++ database/database.js
@@ -11,5 +11,4 @@ const deleteBook = (id) => db.none('DELETE FROM books WHERE ID = $1', [id])
 
 const updateBook = (id, title, author, genre, image, description) => db.none('UPDATE books SET (title, author, genre, image, description) = ($2, $3, $4, $5, $6) WHERE id = $1', [id, title, author, genre, image, description])
 
-
 module.exports = { createBook, getAllBooks, getBook, deleteBook, updateBook }
